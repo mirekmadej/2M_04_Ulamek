8f68056e0bff753d7da8ae1d153e979235239e04
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-09-29 11:14:28 +0200

    getter i setter II wersja

--- 2M_04_Ulamek/Program.cs
+++ 2M_04_Ulamek/Program.cs
@@ -2,25 +2,45 @@
 
 namespace _2M_04_Ulamek
 {
+    class liczba
+    {
+        private int _id;
+        public int getId()
+        {
+            return _id;
+        }
+        private void setId(int id)
+        {
+            this._id = id;
+        }
+    }
     class Ulamek
     {
-        private int licznik=0, mianownik=1;
+        //private int licznik=0, mianownik=1;
+        public int licznik { get; set; }
+        private int _mianownik;
+        public int mianownik {
+            get
+            {
+                return this._mianownik;
+            }
+            set 
+            {
+                if(mianownik == 0)
+                    this._mianownik = 1;
+                else
+                    this._mianownik = mianownik;
 
+            } 
+        }
+        
         public Ulamek(int licznik=0, int mianownik=1)
         {
             int nwd = NWD(licznik, mianownik);
             this.licznik = licznik/nwd;
-            this.mianownik = mianownik/nwd;
+            this._mianownik = mianownik/nwd;
         }
 
-        public int getLicznik()
-        {
-            return licznik;
-        }
-        public void setLicznik(int licznik)
-        {
-           this.licznik = licznik;           
-        }
         public void wypiszUlamek()
         {
             Console.WriteLine($"{licznik}/{mianownik}");
@@ -50,6 +70,11 @@ namespace _2M_04_Ulamek
             u2.wypiszUlamek();
             Ulamek u3 = new Ulamek(3,6);
             u3.wypiszUlamek();
+            u3.licznik = 10;
+            u3.wypiszUlamek();
+            Console.WriteLine(u3.licznik);
+
+     
         }
     }
 }
